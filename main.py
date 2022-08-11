a2d05b08ff3f94d973d3a315dffa91df09edac1b
alamin <alamin.education1997@gmail.com>
2022-08-11 23:30:56 +0600

    face detection

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import mediapipe as mp
 cam = cv2.VideoCapture(0)
 face_mash = mp.solutions.face_mesh.FaceMesh()
 while True:
-    __,frame = cam.read()
+    _,frame = cam.read()
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mash.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
+    frame_h ,frame_w, _ = frame.shape
     if landmark_points:
         landmarks = landmark_points[0].landmark
         for landmark in landmarks:
-            x = landmark.x
-            y = landmark.y
+            x = int(landmark.x*frame_w)
+            y = int(landmark.y*frame_h)
+            cv2.circle(frame , (x,y), 3 , (0,255,0))
             print(x,y)
-
     print(landmark_points)
     cv2.imshow('eye mouse',frame)
     cv2.waitKey(1)
