c18514bcbb5560038cf41c7527841450e357ea2b
alamin <alamin.education1997@gmail.com>
2022-08-11 23:15:42 +0600

    x , y axis update

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mash.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+        for landmark in landmarks:
+            x = landmark.x
+            y = landmark.y
+            print(x,y)
+
     print(landmark_points)
     cv2.imshow('eye mouse',frame)
     cv2.waitKey(1)
